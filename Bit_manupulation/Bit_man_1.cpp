c443e06c8b27035af48e0accb76ebcc4d9a188af
Bhaveshg4 <iambhaveshgupta@gmail.com>
2023-09-06 17:49:07 +0530

    Fourth commit

--- Bit_manupulation/Bit_man_1.cpp
+++ Bit_manupulation/Bit_man_1.cpp
@@ -1,16 +1,13 @@
 #include<iostream>
 using namespace std;
 int getBit(int n,int pos){
-
+    
     return (n & (1<<pos))!=0;
 }
 //unset bit: we have to get 0 to the required bit we have to do | or
 //set bit: we have to get 1 to the required bit
 int setBit(int n,int pos){
-    return (n|(1<<pos))
-   
-
-}
+    return (n|(1<<pos));}
 int main(){
     cout<<getBit(5,2)<<endl;
     cout<<setBit(6,3)<<endl;

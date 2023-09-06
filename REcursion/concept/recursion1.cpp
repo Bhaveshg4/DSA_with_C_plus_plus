dd1e440e74c9285f1998162b73652486f6a5f0fa
Bhaveshg4 <iambhaveshgupta@gmail.com>
2023-09-06 20:17:59 +0530

    understanding recursion

--- REcursion/concept/recursion1.cpp
+++ REcursion/concept/recursion1.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 using namespace std;
+//When a function calls itself to solve a problem is called recursion
+int SUm(int n){
+    if(n=0){return 0;}
+    int prevSum = SUm(n-1);
+    return n+prevSum;
+}
 int main(){
-    
+   int n;
+   cin>>n;
+   cout<<SUm(n)<<endl;
     return 0;
 }
